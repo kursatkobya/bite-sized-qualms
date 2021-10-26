b5f5dd3989b8f72391e75365777aed20854e3691
Kursat Kobya <kursat.kobya@gmail.com>
2021-10-26 22:12:55 +0200

    Add solution to problem

--- concurrent-write/src/main.rs
+++ concurrent-write/src/main.rs
@@ -1,12 +1,21 @@
-use futures::{stream, StreamExt};
+// Problem : https://stackoverflow.com/q/69724691/1087681
+// How to collect multiple results from concurrently working loop?
+
+use futures::{
+    stream, StreamExt,
+    lock::Mutex,
+};
 use std::{
     time::Duration,
     thread,
+    sync::Arc,
+    time::Instant,
 };
 
 #[tokio::main]
 async fn main() {
-    let inputs = stream::iter(0..=u16::MAX);
+    let inputs = stream::iter(0..=1000);
+    let before = Instant::now();
 // This block works
 //     {
 //         inputs
@@ -15,34 +24,42 @@ async fn main() {
 //     }
 
 // This block also works
-//     inputs
-//         .for_each_concurrent(1024, |input| async move {
-//             let result = second_try(input).await;
-//                 if result > 0 {
-//                     println!("{}", result)
-//                 }
-//         }).await;
+// inputs
+//     .for_each_concurrent(1024, |input| async move {
+//         let result = second_try(input).await;
+//             if result > 0 {
+//                 println!("{}", result)
+//             }
+//     }).await;
 
-// Finaly the one below does not work
-    let mut output_values:Vec<u16> = Vec::new();
+// Final solution
+    let output_values = Arc::new(Mutex::new(Vec::new()));
     inputs
-        .for_each_concurrent(1024, |input| async move {
-            let result = second_try(input).await;
-                if result > 0 {
-                    output_values.push(result);
-                }
-        }).await;
+        .for_each_concurrent(1024, |input| {
+            let output_values = output_values.clone();
 
+            async move {
+                let result = test_with_retval(input).await;
+                if result > 0 {
+                    output_values.lock().await.push(result);
+                }
+            }
+        })
+        .await;
+
+    output_values.lock().await.iter().for_each(|x| println!("{:?}", x));
+
+    println!("Elapsed time: {:.2?}", before.elapsed());
 }
 
-async fn test1(input: u16) {
+async fn test(input: u16) {
     thread::sleep(Duration::from_millis(200));
     if input%10 == 0 {
         println!("{}", input)
     }
 }
 
-async fn second_try(input: u16)-> u16{
+async fn test_with_retval(input: u16) -> u16{
     thread::sleep(Duration::from_millis(200));
     if input%10 == 0 {
         return input;
